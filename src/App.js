cb6bd75ee752af5bc4c32928fbbf37dcabe5f6bf
Konrad <konrad.gmur@gmail.com>
2020-09-04 19:17:46 +0200

    Adding button and input to component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,40 @@ import React, { Component } from "react";
 import "./App.css";
 
 class ToDoList extends Component {
+  state = {
+    tasks: this.props.tasks,
+    draft: "",
+  };
+
+  updateDraft = (e) => {
+    this.setState({
+      draft: e.target.value,
+    });
+  };
+
+  addToDo = () => {
+    const { tasks, draft } = this.state;
+    const list = tasks;
+    list.push(draft);
+    this.setState({
+      tasks: list,
+      draft: "",
+    });
+  };
+
   render() {
+    const { title } = this.props;
+    const { tasks, draft } = this.state;
     return (
       <div>
-        <h1>{this.props.title}</h1>
-        {this.props.tasks.map((task) => (
+        <h1>{title}</h1>
+        {tasks.map((task) => (
           <div>
             <p>{task}</p>
           </div>
         ))}
-        <input type="text" />
+        <input type="text" onChange={this.updateDraft} value={draft} />
+        <button onClick={this.addToDo}>Add</button>
       </div>
     );
   }
