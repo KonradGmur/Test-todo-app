7413286ffc1d9f5e0cf9dfcedc86c2e57c656350
Konrad <konrad.gmur@gmail.com>
2020-09-04 18:52:25 +0200

    A second component has been added using props

--- src/App.js
+++ src/App.js
@@ -6,17 +6,26 @@ class ToDoList extends Component {
     return (
       <div>
         <h1>{this.props.title}</h1>
+        {this.props.tasks.map((task) => (
+          <div>
+            <p>{task}</p>
+          </div>
+        ))}
+        <input type="text" />
       </div>
     );
   }
 }
 
-function App() {
-  return (
-    <div>
-      <ToDoList title="My app" />
-    </div>
-  );
+class App extends Component {
+  myTask = ["Record a ReactJS video", "Go for a walk"];
+  render() {
+    return (
+      <div>
+        <ToDoList title="My app" tasks={this.myTasks} />
+      </div>
+    );
+  }
 }
 
 export default App;
