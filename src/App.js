579cde3ea18891dd3c6e88103837c77f49bdc855
Konrad <konrad.gmur@gmail.com>
2020-09-08 20:37:40 +0200

    Adding exact paths to the url

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from "react";
 import ToDoList from "./containers/ToDoList/index";
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import ToDoEditForm from "./components/ToDoEditForm/index.js";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ToDoEditForm from "./components/ToDoEditForm/index";
+import styled from "styled-components";
+import NotFound from "./components/NotFound/index";
+
+const Container = styled.div`
+  background: #2b2e39;
+  margin: 0 auto;
+  width: 80%;
+  max-width: 600px;
+  padding: 14px;
+  border-radius: 14px;
+  margin-top: 14px;
+`;
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Route path="/" component={ToDoList} />
-        <Route path="/servers/:itemId" component={ToDoEditForm} />
+        <Container>
+          <Switch>
+            <Route exact path="/" component={ToDoList} />
+            <Route exact path="/servers/:itemId" component={ToDoEditForm} />
+            <Route component={NotFound} />
+          </Switch>
+        </Container>
       </Router>
     );
   }
