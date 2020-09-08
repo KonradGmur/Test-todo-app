00a4211fc1b865cd5bf45985544870e78c835bb2
Konrad <konrad.gmur@gmail.com>
2020-09-08 20:38:18 +0200

    Adding a path with an incorrect address

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          No match for <code>{this.props.location.pathname}</code>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default NotFound;
