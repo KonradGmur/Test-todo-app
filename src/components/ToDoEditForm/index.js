e5793fa06a90914ac0d4106caa6b3e95b68fdc48
Konrad <konrad.gmur@gmail.com>
2020-09-09 19:32:20 +0200

    Form handling in Formik

--- src/components/ToDoEditForm/index.js
+++ src/components/ToDoEditForm/index.js
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
+import { get } from "../../helpers/toDoItemApi";
 
 class ToDoEditForm extends Component {
+  state = {
+    toDoItem: null,
+    fetched: false,
+  };
+
+  componentDidMount = async () => {
+    const toDoItem = await get(this.props.match.params.itemId);
+    console.log(toDoItem);
+    this.setState({ toDoItem, fetched: true });
+  };
   render() {
-    return <div>Edit form for {this.props.match.params.itemId}</div>;
+    return (
+      <div>
+        Edit form for {this.props.match.params.itemId}
+        {this.state.fetched ? <p>Item fetched</p> : <p>Loading...</p>}
+      </div>
+    );
   }
 }
 
