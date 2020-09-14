cf02ef2ed3561a03f67288e484d3b14746a907a8
Konrad <konrad.gmur@gmail.com>
2020-09-14 20:46:51 +0200

    Support for logging in by FB

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -2,11 +2,32 @@ import React, { Component } from "react";
 import { SubmitButton } from "../../helpers/theme";
 
 class Login extends Component {
+  state = {
+    processing: false,
+  };
+
+  fbLogin = () => {
+    this.setState({ processing: true });
+    window.FB.getLoginStatus((response) => {
+      console.log(response);
+      if (response.status !== "connected") {
+        window.FB.login();
+      } else {
+        window.FB.api("/me", (user) => {
+          console.log(user);
+        });
+      }
+    });
+  };
   render() {
     return (
       <div>
         <p>You must login to view page</p>
-        <SubmitButton>Facebook login</SubmitButton>
+        {this.state.processing ? (
+          <div>Authenticating...</div>
+        ) : (
+          <SubmitButton ocClick={this.fbLogin}>Facebook login</SubmitButton>
+        )}
       </div>
     );
   }
