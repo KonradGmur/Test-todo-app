8ef4c81704a5b52987ca46ce484faa4079308128
Konrad <konrad.gmur@gmail.com>
2020-09-15 20:13:54 +0200

    Adding a Context component to the project part 3

--- src/containers/Navbar/index.js
+++ src/containers/Navbar/index.js
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
+import { CurrentUserConsumer } from "../../context/CurrentUser.context";
 
 class Navbar extends Component {
   render() {
-    return <div>Hello!</div>;
+    return (
+      <CurrentUserConsumer>
+        {({ user, logout }) => (
+          <div>
+            {user ? (
+              <div>
+                Hello {user.name}. <button onClick={logout}>Logout</button>{" "}
+              </div>
+            ) : (
+              <div>Please login...</div>
+            )}
+          </div>
+        )}
+      </CurrentUserConsumer>
+    );
   }
 }
 
