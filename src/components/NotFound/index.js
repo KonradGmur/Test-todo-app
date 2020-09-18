1ef3b24bf5a4b551d79d2831d4b2d5f7d7cdb8cf
Konrad <konrad.gmur@gmail.com>
2020-09-18 19:03:28 +0200

    Using useState and useEffect in a project part 4

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -3,56 +3,23 @@ import { Redirect } from "react-router-dom";
 
 const NotFound = ({ location }) => {
   const [counter, setCounter] = useState(10);
-  const [intervalId, setIntervalId] = useState();
   const countdown = () => setCounter(counter - 1);
 
   useEffect(() => {
-    const interval = setInterval(countdown, 1000);
-    setIntervalId(interval);
-  });
+    const id = setTimeout(countdown, 1000);
+
+    return () => clearTimeout(id);
+  }, [counter]);
 
   return (
     <div onClick={countdown}>
       <p>
-        No match for <code>{this.props.location.pathname}</code>
+        No match for <code>{location.pathname}</code>
       </p>
-      <p>Redirect tot homepage in {this.state.counter} seconds</p>
+      <p>Redirect tot homepage in {counter} seconds...</p>
       {counter === 0 && <Redirect to="/" />}
     </div>
   );
 };
 
 export default NotFound;
-
-// class NotFound extends Component {
-//   state = {
-//     counter: 10,
-//   };
-
-//   componentDidMount = () => {
-//     const intervalId = setInterval(this.countdown, 1000);
-//     this.setState({ intervalId });
-//   };
-
-//   countdown = () => {
-//     this.setState({ counter: this.state.counter - 1 });
-//   };
-
-//   componentWillUnmount = () => {
-//     clearInterval(this.state.intervalId);
-//   };
-
-//   render() {
-//     const { location } = this.props;
-//     const { counter } = this.state;
-//     return (
-//       <div>
-//         <p>
-//           No match for <code>{this.props.location.pathname}</code>
-//         </p>
-//         <p>Redirect tot homepage in {this.state.counter} seconds</p>
-//         {counter === 0 && <Redirect to="/" />}
-//       </div>
-//     );
-//   }
-// }
