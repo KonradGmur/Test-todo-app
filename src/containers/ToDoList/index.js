edd271d090164c9060edd7236d0cf870a0969430
Konrad <konrad.gmur@gmail.com>
2020-09-19 11:50:52 +0200

    Using useReducer in a project

--- src/containers/ToDoList/index.js
+++ src/containers/ToDoList/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import ToDoItem from "../../components/ToDoItem/index";
 import NewTodoForm from "../../components/NewTodoForm/index";
 import styled from "styled-components";
@@ -17,89 +17,116 @@ const DestroyButton = styled.button`
   margin-bottom: 10px;
 `;
 
-class ToDoList extends Component {
-  componentDidMount = async () => {
-    const tasks = await ToDoItemApi.getAll();
-    this.setState({ tasks });
-  };
-
-  static defaultProps = {
-    tasks: [],
-    title: "Todo list",
-  };
-
-  state = {
-    tasks: this.props.tasks,
-    draft: "",
-  };
-
-  updateDraft = (e) => {
-    this.setState({
-      draft: e.target.value,
-    });
-  };
-
-  addToDo = async () => {
-    const { tasks, draft } = this.state;
-    const task = await ToDoItemApi.create({ content: draft });
-
-    this.setState({
-      tasks: _.append(task, tasks),
-      draft: "",
-    });
-  };
-
-  removeAll = () => {
-    this.setState({ tasks: [] });
-  };
-
-  findById = (id, arr) => {
-    const index = _.findIndex(_.propEq("id", id))(arr);
-
-    return { index, task: arr[index] };
-  };
-
-  destroyToDo = async (id) => {
-    const { tasks } = this.state;
-    await ToDoItemApi.destroy(id);
-    const { index } = this.findById(id, tasks);
-
-    this.setState({ tasks: _.remove(index, 1, tasks) });
-  };
-
-  toggleDone = async (id) => {
-    const { tasks } = this.state;
-    const { index, task } = this.findById(id, tasks);
-    const response = await ToDoItemApi.update(id, { done: !task.done });
-
-    this.setState({ tasks: _.update(index, response, tasks) });
-  };
-
-  render() {
-    const { title } = this.props;
-    const { tasks, draft } = this.state;
-    return (
-      <div>
-        <Header>{title}</Header>
-        <DestroyButton onClick={this.removeAll}>Remove all</DestroyButton>
-        {tasks.map((task) => (
-          <ToDoItem
-            destroy={this.destroyToDo}
-            key={task.id}
-            id={task.id}
-            text={task.name}
-            toggleDone={this.toggleDone}
-            done={task.done}
-          />
-        ))}
-        <NewTodoForm
-          onSubmit={this.addToDo}
-          onChange={this.updateDraft}
-          draft={draft}
+const ToDoList = () => {
+  const [draft, setDraft] = useState("");
+  return (
+    <div>
+      <Header>{title}</Header>
+      <DestroyButton onClick={this.removeAll}>Remove all</DestroyButton>
+      {tasks.map((task) => (
+        <ToDoItem
+          destroy={this.destroyToDo}
+          key={task.id}
+          id={task.id}
+          text={task.name}
+          toggleDone={this.toggleDone}
+          done={task.done}
         />
-      </div>
-    );
-  }
-}
+      ))}
+      <NewTodoForm
+        onSubmit={this.addToDo}
+        onChange={this.updateDraft}
+        draft={draft}
+      />
+    </div>
+  );
+};
 
 export default ToDoList;
+
+// class ToDoList extends Component {
+//   componentDidMount = async () => {
+//     const tasks = await ToDoItemApi.getAll();
+//     this.setState({ tasks });
+//   };
+
+//   static defaultProps = {
+//     tasks: [],
+//     title: "Todo list",
+//   };
+
+//   state = {
+//     tasks: this.props.tasks,
+//     draft: "",
+//   };
+
+//   updateDraft = (e) => {
+//     this.setState({
+//       draft: e.target.value,
+//     });
+//   };
+
+//   addToDo = async () => {
+//     const { tasks, draft } = this.state;
+//     const task = await ToDoItemApi.create({ content: draft });
+
+//     this.setState({
+//       tasks: _.append(task, tasks),
+//       draft: "",
+//     });
+//   };
+
+//   removeAll = () => {
+//     this.setState({ tasks: [] });
+//   };
+
+//   findById = (id, arr) => {
+//     const index = _.findIndex(_.propEq("id", id))(arr);
+
+//     return { index, task: arr[index] };
+//   };
+
+//   destroyToDo = async (id) => {
+//     const { tasks } = this.state;
+//     await ToDoItemApi.destroy(id);
+//     const { index } = this.findById(id, tasks);
+
+//     this.setState({ tasks: _.remove(index, 1, tasks) });
+//   };
+
+//   toggleDone = async (id) => {
+//     const { tasks } = this.state;
+//     const { index, task } = this.findById(id, tasks);
+//     const response = await ToDoItemApi.update(id, { done: !task.done });
+
+//     this.setState({ tasks: _.update(index, response, tasks) });
+//   };
+
+//   render() {
+//     const { title } = this.props;
+//     const { tasks, draft } = this.state;
+//     return (
+//       <div>
+//         <Header>{title}</Header>
+//         <DestroyButton onClick={this.removeAll}>Remove all</DestroyButton>
+//         {tasks.map((task) => (
+//           <ToDoItem
+//             destroy={this.destroyToDo}
+//             key={task.id}
+//             id={task.id}
+//             text={task.name}
+//             toggleDone={this.toggleDone}
+//             done={task.done}
+//           />
+//         ))}
+//         <NewTodoForm
+//           onSubmit={this.addToDo}
+//           onChange={this.updateDraft}
+//           draft={draft}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+// export default ToDoList;
