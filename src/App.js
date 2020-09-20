e09b53aec46a7eb5048dd4d0dc42104549e673d0
Konrad <konrad.gmur@gmail.com>
2020-09-20 15:11:44 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, lazy, Suspense } from "react";
 import ToDoList from "./containers/ToDoList/index";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -12,6 +12,8 @@ import {
   CurrentUserProvider,
   CurrentUserConsumer,
 } from "./context/CurrentUser.context";
+
+const Stats = lazy(() => import("./containers/Stats/index"));
 
 const Container = styled.div`
   background: #2b2e39;
@@ -49,21 +51,25 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <CurrentUserProvider>
-          <Container>
-            <Navbar />
-            <Switch>
-              <Route exact path="/" component={ToDoList} />
-              <PrivateRoute
-                exact
-                path="/servers/:itemId"
-                component={ToDoEditForm}
-              />
-              <Route component={NotFound} />
-              <Route exact path="/login" component={Login} />
-            </Switch>
-          </Container>
-        </CurrentUserProvider>
+        <Suspense fallback={<div>Loading...</div>}>
+          <CurrentUserProvider>
+            <Container>
+              <Navbar />
+              <Switch>
+                <Route exact path="/" component={ToDoList} />
+                <PrivateRoute
+                  exact
+                  path="/servers/:itemId"
+                  component={ToDoEditForm}
+                />
+                <Route component={NotFound} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/stats" component={Stats} />
+                <Route component={NotFound} />
+              </Switch>
+            </Container>
+          </CurrentUserProvider>
+        </Suspense>
       </Router>
     );
   }
