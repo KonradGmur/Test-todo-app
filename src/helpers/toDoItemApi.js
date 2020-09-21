24a31188f089b25adaaa59e69b162fa27d66e30e
Konrad <konrad.gmur@gmail.com>
2020-09-21 20:06:04 +0200

    Update helpers - toDoItemApi.js

--- src/helpers/toDoItemApi.js
+++ src/helpers/toDoItemApi.js
@@ -5,10 +5,10 @@ export const getAll = () => api.get(toDoItemsApiUrl());
 
 export const get = (id) => api.get(toDoItemsApiUrl(id));
 
+export const update = (id, params) =>
+  api.put(toDoItemsApiUrl(id), { todo_item: { ...params } });
+
 export const create = (params) =>
-  api.post(toDoItemsApiUrl(), { servers: { ...params } });
+  api.post(toDoItemsApiUrl(), { todo_item: { ...params } });
 
 export const destroy = (id) => api.destroy(toDoItemsApiUrl(id));
-
-export const update = (id, params) =>
-  api.put(toDoItemsApiUrl(id), { servers: { ...params } });
